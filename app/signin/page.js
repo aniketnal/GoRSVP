a56b970bf97d3d08e9d030b23625e29fada22419
Ayush Wankhede <ayushwankhede9@gmail.com>
2024-12-03 23:44:23 +0530

    added-signin-page

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-sm bg-white p-6 rounded-sm shadow-lg">
+        <h2 className="text-4xl font-bold text-center text-red-700">Sign In</h2>
+        <form className="mt-4">
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="123@abc.com"
+              name="email"
+              className="mt-1 w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              required
+            />
+          </div>
+          <div className="mb-4 relative">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-1 w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-red-500"
+              required
+            />
+          </div>
+          <div className="flex justify-end">
+            <a
+              href="#"
+              className="text-red-600 hover:underline hover:text-red-700"
+            >
+              Forgot password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="mt-6 w-full px-4 py-2 text-white bg-red-800 rounded-md shadow-sm focus:shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+          >
+            Sign In
+          </button>
+        </form>
+        <p className="mt-6 text-center text-gray-600">
+          Don't have an account?{' '}
+          <a
+            href="#"
+            className="text-red-600 hover:underline hover:text-red-700"
+          >
+            Sign Up
+          </a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
