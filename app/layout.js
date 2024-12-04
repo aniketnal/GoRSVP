f668a51f2f765bc65205e9e850949d607e689c5a
aniketnal <aniketnalegaonkar44@gmail.com>
2024-12-04 11:06:45 +0530

    Move Navbar component from page.js to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Poppins } from 'next/font/google'
+import Navbar from "@/components/Navbar";
  
 const poppins = Poppins({
   weight: '400',
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
         {children}
       </body>
     </html>
