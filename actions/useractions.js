5dd7c7b9b1568172db2fdc857fd2cbb205577990
Anshul Singh <anshulsingh024@yahoo.com>
2025-01-10 21:34:17 +0530

    typos

--- actions/useractions.js
+++ actions/useractions.js
@@ -5,10 +5,11 @@ import User from "@/models/User"
 import Event from "@/models/Event"
 import Contact from "@/models/Contact"
 
+//contact us form submission, POST req.
 export const savecontact = async (msg) => {
     await connectDB();
     
-    console.log(msg);
+    // console.log(msg);
     const newcontact = new Contact({
         name: msg.name,
         email: msg.email,
