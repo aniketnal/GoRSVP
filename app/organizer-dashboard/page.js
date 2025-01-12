17f819ae569be06f59a9f97ba93e61c40c4838d7
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-12 16:04:02 +0530

    modified organizer dashboard and added redirection to the page

--- app/organizer-dashboard/page.js
+++ app/organizer-dashboard/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import { CalendarCheck } from 'lucide-react';
 
 const page = () => {
   const generateSampleData = () => {
@@ -60,53 +61,52 @@ const page = () => {
     { field: 'lastname', headerName: 'Last Name', width: 220 },
     { field: 'status', headerName: 'Status', width: 220 },
     { field: 'email', headerName: 'Email', width: 240 },
-    { field: 'events', headerName: 'Events Conducted', width: 220 },
+    { field: 'events', headerName: 'Events Hosted', width: 220 },
     
   ];
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-foregorund">
       {/* Event Dashboard */}
       <div className="flex justify-between mx-4">
-        <h2 className="text-xl mt-4 font-bold">Organizer's Dashboard</h2>
-        <button
-          type="button"
-          className="mt-4 text-white bg-primary rounded-md shadow-sm focus:shadow-md hover:bg-secondary w-fit p-2"
-        >
-          <Link href="./createevent">+ Create Event</Link>
-        </button>
+        <h2 className="text-primary ml-8 text-xl mt-4 font-bold">Organizer's Dashboard</h2>
       </div>
       {/* Cards */}
-      <div className="flex justify-center gap-40 mt-8">
-        <div className="bg-white p-4 rounded-lg flex shadow hover:shadow-lg w-1/3">
-          <aside>LOGO HERE</aside>
+      <div className="flex justify-center gap-36 mt-8">
+        <div className="bg-[rgb(249,248,240)] p-4 rounded-lg flex shadow-lg w-1/3">
+          <aside> <CalendarCheck className="text-primary" /></aside>
           <div className="ml-8">
-            <h2 className="text-lg font-semibold text-gray-800">
+            <h2 className="text-lg font-semibold text-gray-800 flex items-end">
               Total Events
             </h2>
             <p className="text-lg text-black mt-2">100</p>
           </div>
         </div>
-        <div className="bg-white p-3 flex rounded-lg shadow hover:shadow-lg w-1/3">
-          <aside>LOGO HERE</aside>
-          <div className="ml-4 flex flex-col w-full">
-            <h2 className="text-lg font-semibold text-gray-800">
-              Total RSVP's
-            </h2>
-            <p className="text-lg text-black mt-2">71</p>
-            <button className="border border-black p-1 text-sm bg-slate-200 mt-auto self-end">
+        <div className="bg-[rgb(249,248,240)] p-4 rounded-lg flex shadow-lg w-1/3">
+          <aside> <CalendarCheck className="text-primary" /></aside>
+          <div className="ml-8 flex justify-between w-full items-end">
+            <div>
+              <h2 className="text-lg font-semibold text-gray-800">
+                Total RSVP's
+              </h2>
+              <p className="text-lg text-black mt-2">300</p>
+            </div>
+           <div>
+           <button className="border-2 border-secondary rounded-md  px-2 py-1 hover:bg-secondary hover:text-footertext text-sm bg-foregorund text-secondary mt-auto self-end">
               view
             </button>
+           </div>
           </div>
         </div>
       </div>
 
-      <div className="border-b-2 border-gray-200 mt-6"></div>
+      <div className="border-b-2 mt-6"></div>
       {/* Your Events Section */}
-      <div className="min-h-screen min-w-full bg-gray-50 p-4">
+      <div className="min-h-screen bg-foreground min-w-full p-4">
             {/* Main Content */}      
-            <div style={{ height: 500, width: '90%', displayflex: 'flex', justifyContent: 'center', margin: 'auto' }}>
-            <DataGrid rows={rows} columns={columns} />
+            <div className="text-primary text-2xl ml-8 mb-10">My Events</div>
+            <div style={{ height: 600, width: '90%', displayflex: 'flex', justifyContent: 'center', margin: 'auto' }}>
+            <DataGrid className="bg-white" rows={rows} columns={columns} />
             </div>
             
       </div>
